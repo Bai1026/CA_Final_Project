4dfb8fb985148d2c26da84a01a45a221880cb6f3
PtrickL <tingxulo0620@gmail.com>
2025-06-05 23:47:06 +0800

    Update matrix_chain_multiplication.s

--- Final/matrix_chain_multiplication.s
+++ Final/matrix_chain_multiplication.s
@@ -1,260 +1,252 @@
 .text
 .globl matrix_chain_multiplication
+.globl malloc
+
+
+#	a0: matrices, int**
+#	a1: rows, 	int*
+#	a2: cols,	int*
+#	a3: count,	int
 
 matrix_chain_multiplication:
-    addi sp, sp, -2048
-    sw ra, 0(sp)
+	sw ra, 0(sp)
+	addi s0, a3, 1		#	s0 = n+1
+	addi s1, x0, 0
+	addi s2, x0, 0
+	addi s3, x0, 0
+	addi a1, a1, -4		#	shift elements in a1 right by 1, first element = a1[1]
+	addi a2, a2, -4		#	shift elements in a2 right by 1 
+	addi sp, sp, -1088	#	table m, m[i, j] = (i*16+j)*4
+	mv t0, sp
+	mv a7, a0			#	a7 = matrix value address
+init_tablem:
+	addi s1, s1, 1
+	slli s2, s1, 4
+	add s3, s1, s2
+	slli s3, s3, 2
+	add t0, sp, s3
+	sw zero, 0(t0)
+	bne s1, a3, init_tablem
+	
+	mv t0, sp			#	t0 = table m address
+	addi sp, sp, -1088	#	table s
+	li t2, 0x7fffffff	#	t2 = max number	
+	mv t1, sp			#	t1 = table s address
+	
+start_DP:
+	addi s1, zero, 2		#	s1 = l
+DP_outer:
+	addi s2, zero, 1		#	s2 = i = 1
+DP_inter:
+	sub a4, a3, s1
+	addi a4, a4, 2		#	a4 = n-l+2
+	add s3, s1, s2
+	addi s3, s3, -1		#	s3 = j = i+l-1
+	slli s4, s2, 4
+	add s4, s4, s3
+	slli s4, s4, 2		#	s4 = [i, j] offset
+	add t5, t0, s4		#	t5 = m[i, j] address
+	sw t2, 0(t5)
+	add s4, zero, s2		#	s4 = k
+DP_inner:
+	slli s5, s2, 4
+	add s5, s5, s4
+	slli s5, s5, 2		#	s5 = [i, k] offset
+	addi s6, s4, 1
+	slli s6, s6, 4
+	add s6, s6, s3
+	slli s6, s6, 2		#	s6 = [k+1, j] offset
+	add t3, t0, s5
+	add t4, t0, s6
+	lw s5, 0(t3)		#	s5 = m[i, k]
+	lw s6, 0(t4)		#	s6 = m[k+1, j]
+	slli s7, s7, 2		#	s7 = i offset
+	slli s8, s4, 2		#	s8 = k offset
+	slli s9, s3, 2		#	s9 = j offset
+	add s10, a2, s8
+	add s11, a2, s9	
+	lw s8, 0(s10)		#	s8 = p_k
+	lw s9, 0(s11)		#	s9 = p_j
+	add s10, a1, s7
+	lw s7, 0(s10)		#	s7 = p_i-1
+	mul s7, s7, s8
+	mul s7, s7, s9		#	s7 = p_i-1*p_k*p_j
+	add s6, s6, s7
+	add s5, s5, s6		#	s5 = q
+	lw s8, 0(t5)		#	s8 = m[i, j]
+	
+	slt s6, s5, s8
+	beq s6, x0, else_q	#	if q < m[i, j]:
+	addi t6, t5, -1088	#		t6 = s[i, j] address
+	sw s5, 0(t5)		#		m[i, j] = q
+	sw s4, 0(t6)		#		s[i, j] = k
+	
+else_q:
+	addi s4, s4, 1
+	bne s4, s3, DP_inner
 
-    mv s0, a0    # s0 = matrices
-    mv s1, a1    # s1 = rows
-    mv s2, a2    # s2 = cols
-    mv s3, a3    # s3 = count = N
+	addi s2, s2, 1
+	bne s2, a4, DP_inter
+	
+	addi s1, s1, 1
+	bne s1, s0, DP_outer
+	
+end_DP:
+	addi s0, x0, 1		#	s0 = i = 1
+	add s1, x0, a3		#	s1 = j = n
+	addi s2, x0, 0		#	s2 = left matrix address
+	addi s3, x0, 0		#	s3 = right matrix address
+	addi s4, x0, 0		#	s4 = row[left] 
+	addi s5, x0, 0		#	s5 = col[left] = row[right]
+	addi s6, x0, 0		#	s6 = col[right]
+	mv t1, sp			#	t1 = table s address = original sp-2176
+						#	a1: rows, 	int*
+						#	a2: cols,	int*
+						#	a3: count,	int
+						#	a7: matrices, int**
 
-    li s4, 0     # i = 0
-fill_diagonal:
-    bge s4, s3, finish_fill_diag
-    slli t0, s4, 6
-    add t1, sp, t0
-    li t2, 0
-    sw t2, 0(t1)
-    addi s4, s4, 1
-    j fill_diagonal
-finish_fill_diag:
+	
+	jal ra, optimal_order
+	
+	addi sp, sp, 1088
+	addi sp, sp, 1088
+	lw ra, 0(sp)
+	jr ra
 
-    li s5, 2     # chain length L
-outer_loop:
-    bgt s5, s3, done_dp
-    li s6, 0     # i = 0
-inner_i_loop:
-    sub t0, s3, s5
-    bgt s6, t0, next_L
-    add s7, s6, s5
-    addi s7, s7, -1  # j = i + L - 1
+optimal_order:
+	addi sp, sp, -32
+	sw s0, 0(sp)
+	sw s1, 4(sp)
+	sw s2, 8(sp)		
+	sw s3, 12(sp)
+	sw s4, 16(sp)
+	sw s5, 20(sp)
+	sw s6, 24(sp)
+	sw ra, 28(sp)
+	
+	beq s0, s1, matrix_found
+	
+find_L:
+	slli t3, s0, 4
+	add t3, t3, s1
+	slli t3, t3, 2		#	t3 = [i, j] offset
+	add t3, t1, t3
+	lw s1, 0(t3)		#	s1 = s[i, j]
+	jal ra, optimal_order
+find_R:
+	mv s0, s1			#	s0 = s[i, j]
+	lw s1, 4(sp)		#	s1 = j
+	addi s0, s0, 1		#	s0 = s[i, j]+1
+	jal ra, optimal_order
+	lw s0, 0(sp)		#	s0 = i
+	lw ra, 28(sp)
 
-    li t1, 0x7fffffff
-    slli t2, s6, 6
-    add t2, sp, t2
-    slli t3, s7, 2
-    add t2, t2, t3
-    sw t1, 0(t2)     # m[i][j] = INF
+multiply_matrices:
+	mul a0, s4, s6
+	slli a0, a0, 2
+	jal malloc
+	mv t0, a0			#	t0 = result address (ori t5)
+						# 	s4 = M = rows[i] (ori s3)
+						# 	s6 = N = cols[j] (ori s4)
+						# 	s5 = K = rows[k+1] (ori s5)
 
-    mv s8, s6        # k = i
-k_loop:
-    beq s8, s7, end_k_loop
-
-    # m[i][k]
-    slli t0, s6, 6
-    add t0, sp, t0
-    slli t1, s8, 2
-    add t0, t0, t1
-    lw t2, 0(t0)
-
-    # m[k+1][j]
-    addi t3, s8, 1
-    slli t4, t3, 6
-    add t4, sp, t4
-    slli t5, s7, 2
-    add t4, t4, t5
-    lw t3, 0(t4)
-
-    # rows[i]
-    slli t0, s6, 2
-    add t0, s1, t0
-    lw t4, 0(t0)
-
-    # rows[k+1]
-    addi t0, s8, 1
-    slli t0, t0, 2
-    add t0, s1, t0
-    lw t5, 0(t0)
-
-    # cols[j]
-    slli t0, s7, 2
-    add t0, s2, t0
-    lw t6, 0(t0)
-
-    mul t4, t4, t5
-    mul t4, t4, t6       # cost = rows[i] * rows[k+1] * cols[j]
-    add s9, t2, t3
-    add s9, s9, t4       # total = m[i][k] + m[k+1][j] + cost
-
-    # compare with current m[i][j]
-    slli t0, s6, 6
-    add t0, sp, t0
-    slli t1, s7, 2
-    add t0, t0, t1
-    lw t2, 0(t0)
-    bge t2, s9, update_m_s
-    j skip_update
-update_m_s:
-    sw s9, 0(t0)
-    # s[i][j] = k
-    addi t0, sp, 1024
-    slli t1, s6, 6
-    add t0, t0, t1
-    slli t2, s7, 2
-    add t0, t0, t2
-    sw s8, 0(t0)
-skip_update:
-    addi s8, s8, 1
-    j k_loop
-end_k_loop:
-    addi s6, s6, 1
-    j inner_i_loop
-next_L:
-    addi s5, s5, 1
-    j outer_loop
-done_dp:
-
-    li a0, 0
-    addi a1, s3, -1
-    mv a2, s0      # matrices
-    mv a3, s1      # rows
-    mv a4, s2      # cols
-    addi a5, sp, 1024  # s_table
-    jal ra, multiply_chain	# Call multiply_chain
-    mv a0, a0      # result pointer returned in a0
-
-    lw ra, 0(sp)
-    addi sp, sp, 2047
-	addi sp, sp, 1
-    jr ra
-
-multiply_chain:
-    addi sp, sp, -32
-    sw ra, 0(sp)
-    sw s0, 4(sp)
-    sw s1, 8(sp)
-    sw s2, 12(sp)
-
-    beq a0, a1, base_case
-
-    mv s0, a0    # i
-    mv s1, a1    # j
-
-    mv t0, a5
-    slli t1, s0, 6
-    add t0, t0, t1
-    slli t2, s1, 2
-    add t0, t0, t2
-    lw t3, 0(t0)   # k = s[i][j]
-
-    # Left = multiply_chain(i, k)
-    mv a0, s0
-    mv a1, t3
-    mv a2, a2
-    mv a3, a3
-    mv a4, a4
-    mv a5, a5
-    jal ra, multiply_chain
-    mv s2, a0      # left_matrix_ptr
-
-    # Right = multiply_chain(k+1, j)
-    addi a0, t3, 1
-    mv a1, s1
-    mv a2, a2
-    mv a3, a3
-    mv a4, a4
-    mv a5, a5
-    jal ra, multiply_chain
-    mv t6, a0      # right_matrix_ptr
-
-    # Allocate result matrix of rows[i] × cols[j]
-    slli t0, s0, 2
-    add t0, a3, t0
-    lw t1, 0(t0)      # rows[i]
-    slli t2, s1, 2
-    add t2, a4, t2
-    lw t3, 0(t2)      # cols[j]
-    mul t4, t1, t3
-    slli a0, t4, 2
-    #li a7, 93
-    #ecall
-    mv t5, a0        # result pointer
-
-    # Multiply Left (s2) × Right (t6) into Result (t5)
-    mv s3, t1      # M = rows[i]
-    mv s4, t3      # N = cols[j]
-
-    slli t0, t3, 2 # t0 = N*4 (col step)
-    slli t1, t1, 2 # t1 = M*4 (row step)
-    
-    # rows[k+1]
-    slli t2, t3, 2
-    add t2, a3, t2
-    lw s5, 0(t2)   # K = rows[k+1]
-
-    li s6, 0       # x = 0
+    li t4, 0       		#	t4 = x = 0 (ori s6)
 loop_x:
-    bge s6, s3, done_matmul
-    li s7, 0       # y = 0
+    bge t4, s4, end_x	#	if x == row[left]:	done
+    li t5, 0       		# 	t5 = y = 0 (ori s7)
 loop_y:
-    bge s7, s4, next_x
+    bge t5, s6, end_y	#	if y == col[right]: end_y
 
-    # result[x*N + y] = 0
-    mul t2, s6, s4     # x * N
-    add t2, t2, s7     # + y
-    slli t2, t2, 2     # offset in bytes
-    add t3, t5, t2     # address of result[x*N + y]
-    li t4, 0
-    sw t4, 0(t3)
+    #	Initialize result[x*N + y] = 0
+    mul a0, t4, s6     	# 	a0 = x * N (ori t0)
+    add a0, a0, t5     	# 	a0 += y
+    slli a0, a0, 2     	# 	byte offset
+    add a0, t0, a0     	# 	result[x][y] address
+    sw zero, 0(a0)		#	result[x][y] = 0
 
-    li s8, 0       # z = 0
+    li t6, 0           	# 	t6 = z = 0 (ori s8)
 loop_z:
-    bge s8, s5, next_y
+    bge t6, s5, end_z	#	if z == row[k+1]:	end_z
 
-    # A[x*K + z]
-    mul t0, s6, s5
-    add t0, t0, s8
-    slli t0, t0, 2
-    add t1, s2, t0
-    lw t2, 0(t1)
+    # 	left[x*K+z]
+    mul a4, t4, s5     	# 	a4 = x*K (ori t0)
+    add a4, a4, t6		#	a4 = x*K+z
+    slli a4, a4, 2
+    add a4, s2, a4		#	a4 = left[x][z] address
+    lw a4, 0(a4)       	# 	a4 = left[x][z]
 
-    # B[z*N + y]
-    mul t0, s8, s4
-    add t0, t0, s7
-    slli t0, t0, 2
-    add t1, t6, t0
-    lw t3, 0(t1)
+    # 	right[z*N+y]
+    mul a5, t6, s6
+    add a5, a5, t5
+    slli a5, a5, 2
+    add a5, s3, a5
+    lw a5, 0(a5)       	# 	a5 = right[z][y]
 
-    mul t4, t2, t3
+    mul a6, a4, a5     	# 	a6 = left[x][z]*right[z][y]
 
-    # result[x*N + y] += t4
-    mul t0, s6, s4
-    add t0, t0, s7
-    slli t0, t0, 2
-    add t1, t5, t0
-    lw t2, 0(t1)
-    add t2, t2, t4
-    sw t2, 0(t1)
+    # 	result[x][y] += A[x][z] * B[z][y]
+	lw a4, 0(a0)		#	load result[x][y]
+	add a4, a4, a6		#	result[x][y] += s6
+	sw a4, 0(a0)		#	write back
 
-    addi s8, s8, 1
+    addi t6, t6, 1		#	z++
     j loop_z
-next_y:
-    addi s7, s7, 1
+
+end_z:
+    addi t5, t5, 1		#	y++
     j loop_y
-next_x:
-    addi s6, s6, 1
+
+end_y:
+    addi t4, t4, 1		#	x++
     j loop_x
-done_matmul:
 
-    # Store result pointer in a0
-    mv a0, t5
+end_x:
+	
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw s2, 8(sp)		
+	lw s3, 12(sp)
+	lw ra, 28(sp)
 
-    lw ra, 0(sp)
-    lw s0, 4(sp)
-    lw s1, 8(sp)
-    lw s2, 12(sp)
-    addi sp, sp, 32
-    jr ra
+    #	Store result pointer
+	#beq s3, zero, store_right
+	beq s2, zero, store_left
+store_right:
+	mv s3, t5
+	mv s5, s4
+	lw s4, 16(sp)
+	addi sp, sp, 32
+	jr ra
+store_left:
+    mv s2, t5
+	mv s5, s6
+	lw s6, 24(sp)
+	addi sp, sp, 32
+	jr ra
+#################################################################################
+	
+matrix_found:
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw s2, 8(sp)		
+	lw s3, 12(sp)
+	lw s4, 16(sp)
+	lw s5, 20(sp)
+	lw s6, 24(sp)
+	lw ra, 28(sp)
+	addi sp, sp, 32
 
-base_case:
-    slli t0, a0, 2
-    add t0, a2, t0
-    lw a0, 0(t0)
-    lw ra, 0(sp)
-    lw s0, 4(sp)
-    lw s1, 8(sp)
-    lw s2, 12(sp)
-    addi sp, sp, 32
-    jr ra
+	slli t3, s0, 2		#	t3 = i offset
+	beq s2, x0, L_found
+R_found:
+	add s6, a2, t3		#	s6 be address of col[right]
+	add s3, a7, t3		#	s3 = address of right matrix
+	lw s6, 0(s6)		#	s6 = col[right]
+	jr ra
+L_found:
+	add s2, a7, t3		#	s2 = address of left matrix
+	add s4, a1, t3		#	s4 be address of row[left]
+	add s5, a2, t3		#	s5 be address of col[left]
+	lw s4, 0(s4)		#	s4 = row[left]
+	lw s5, 0(s5)		#	s5 = col[left]
+	jr ra
